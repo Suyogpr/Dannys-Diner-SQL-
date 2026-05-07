917a778ffb7275465f4f27dd2ec543902cae28fc
Suyogpr <ssuyogppradhan@gmail.com>
2026-05-07 20:00:14 +0545

    DD SQL

--- dd.sql
+++ dd.sql
@@ -139,3 +139,6 @@ group by s.customer_id
 --Q10):In the first week after a customer joins the program (including their join date) 
 --they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
+--Bonus Question
+--Join All The Things
+--Rank All The Things
